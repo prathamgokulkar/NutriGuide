14c9b8607accb8e70004289c1c8f19dd76548db8
Prathamesh Gokulkar <prathamgokulkar@gmail.com>
2025-07-28 23:49:46 +0530

    Explore Card created

--- app/components/ExploreCard.jsx
+++ app/components/ExploreCard.jsx
@@ -4,16 +4,23 @@ import Link from "next/link";
 
 const ExploreCard = () => {
   return (
-    <div>
-      <Link href="/input">
-        <img src="./ExploreImage1.jpeg" alt="Explore Recipes" />
-      </Link>
-      <Link href="/input">
-        <img src="./ExploreImage2.jpeg" alt="Explore Recipes" />
-      </Link>
-      <Link href="/input">
-        <img src="./ExploreImage3.jpeg" alt="Explore Recipes" />
-      </Link>
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 object-contain">
+      {[
+        "ExploreImage1.jpeg",
+        "ExploreImage2.jpeg",
+        "ExploreImage3.jpeg",
+        "ExploreImage4.jpeg",
+      ].map((item, index) => (
+        <Link href="/input" key={index}>
+          <div className="w-64 h-64 overflow-hidden rounded-lg shadow-md">
+            <img
+              src={`/${item}`}
+              alt="Explore Recipies"
+              className="w-full h-full"
+            />
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
